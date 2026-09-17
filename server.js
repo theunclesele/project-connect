517902bfa36117f57fb0560fbf395ddf563f909c
theunclesele <theunclesele@gmail.com>
2026-09-17 18:34:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -100,3 +100,4 @@ app.get("/api/health", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Backend running on http://localhost:${PORT}`);
   console.log(`   Send test: POST http://localhost:${PORT}/api/send-test`);
+});
